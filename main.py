b67f82598d54513942322893bcfc87986d4caa30
mirzamomin <mirzamomin2000@gmail.com>
2024-01-17 15:37:36 +0500

    removed instagram scraping and unnecessary package imports

--- main.py
+++ main.py
@@ -6,13 +6,23 @@
     1. Imports 
 """
 
+import sys
 import shutup
 import asyncio
 import requests
+import argparse
+
+if sys.platform == 'win32':
+	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+parser = argparse.ArgumentParser(description='Free proxies fetcher and rotator to scrape websites')
+parser.add_argument('limit',help="Number of Proxies to Fetch")
+args=parser.parse_args()
+
+limit = int(args.limit)
 
 from proxybroker import Broker
-from googleapiclient import discovery
-from google.oauth2 import service_account
+
 
 """
     2. Main Functionality
@@ -42,9 +52,8 @@ async def append_proxies(proxies, proxy_pool): # async function (coroutine funct
                 "http": "{0}:{1}".format(proxy.host, proxy.port),
                 "https": "{0}:{1}".format(proxy.host, proxy.port)
                 })
-            
 
-            
+
 def proxy_refiner():
     """
         Calls append_proxies() asynchronously, refines the proxies returned that don't function and
@@ -60,7 +69,7 @@ def proxy_refiner():
     proxies = asyncio.Queue()
     broker = Broker(proxies)
     tasks = asyncio.gather(
-        broker.find(types=['HTTP', 'HTTPS'], limit=10),
+        broker.find(types=['HTTP', 'HTTPS'], limit=limit),
         append_proxies(proxies, proxy_pool))
 
     loop = asyncio.get_event_loop()
@@ -77,8 +86,36 @@ def proxy_refiner():
     return proxy_pool
 
 
+def export(proxy_pool):
+    """
+        Used to scrape Instagram features as required
+
+        Parameters
+        ----------
+        proxy_pool: list
+            Pool of valid & working proxies that we can use to scrape instragram 
+    """
+    for proxy in proxy_pool:
+        try:
+
+            # Splitting proxy host and port and creating a URL to export to environment variable
+            proxy_host = proxy['http'].split(':')[0]
+            proxy_port = proxy['http'].split(':')[1]
+            proxy_url = f'http://{proxy_host}:{proxy_port}'
+
+            # Exporting valid proxies to proxies.txt for record using File Handling
+            file = open('proxies.txt', 'a')
+            file.write(proxy_url + f'\n')
+            file.close()
+
+        except Exception as e:
+            print('@@@ ERROR: {}\n {}\n\n'.format(proxy, e))
+            print('----------------------------------------------------------------')
+
+
 def main():
-    print(proxy_refiner())
+    proxy_pool = proxy_refiner()
+    export(proxy_pool)
 
 
 if __name__ == '__main__':
